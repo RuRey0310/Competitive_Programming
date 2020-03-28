95ed29223f8ec5cf13268797becf0c44ffe74fa0
RuRey0310 <y.sumitomo310@gmail.com>
2020-03-28 22:55:24 +0900

    abc160

--- ABC151~200/ABC160/e.py
+++ ABC151~200/ABC160/e.py
@@ -7,3 +7,19 @@ p.sort(reverse=True)
 q.sort(reverse=True)
 r.sort(reverse=True)
 
+a = []
+for i in range(x):
+    a.append(p[i])
+
+for i in range(y):
+    a.append(q[i])
+
+for i in range(c):
+    a.append(r[i])
+
+a.sort(reverse=True)
+ans = 0
+for i in range(x + y):
+    ans += a[i]
+
+print(ans)
