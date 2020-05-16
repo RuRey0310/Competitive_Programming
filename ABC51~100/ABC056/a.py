368d76bb96b0474d44cfcde90c820a35b1b7ce93
RuRey0310 <58558604+RuRey0310@users.noreply.github.com>
2020-05-16 21:15:17 +0900

    abc056

---
+++ ABC51~100/ABC056/a.py
@@ -0,0 +1,12 @@
+a, b = map(str, input().split())
+
+if a == "D":
+    if b == "H":
+        print("D")
+    else:
+        print("H")
+else:
+    if b == "H":
+        print("H")
+    else:
+        print("D")
